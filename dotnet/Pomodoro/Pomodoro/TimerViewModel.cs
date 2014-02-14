30983cb1a3269d01d2478a245dc6a11a6feb4858
Roman FILIP <dicky@email.cz>
2014-02-14 22:00:39 +0100

    Added application for Pomodoro

---
+++ dotnet/Pomodoro/Pomodoro/TimerViewModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.ComponentModel;
+using System.Timers;
+using System.Windows.Input;
+using RFI.Pomodoro.Helpers;
+using RFI.Pomodoro.Properties;
+
+namespace RFI.Pomodoro
+{
+    class TimerViewModel : INotifyPropertyChanged
+    {
+        private readonly TimeSpan _workDuration;
+
+        private readonly TimeSpan _breakDuration;
+
+        #region ActualTime
+        private TimeSpan _actualTime;
+
+        public TimeSpan ActualTime
+        {
+            get
+            {
+                return _actualTime;
+            }
+            set
+            {
+                _actualTime = value;
+                RaisePropertyChanged("ActualTime");
+            }
+        }
+        #endregion
+
+        private Timer _timer;
+
+        public TimerViewModel()
+        {
+            _workDuration = Settings.Default.WorkDuration;
+            _breakDuration = Settings.Default.BreakDuration;
+            ActualTime = _workDuration;
+
+            ResetTimerCommand = new DelegateCommand(ResetTimerCommandExecuted, ResetTimerCommandCanExecute);
+            StartPauseTimerCommand = new DelegateCommand(StartPauseTimerCommandExecuted, StartPauseTimerCommandCanExecute);
+
+            _timer = new Timer(TimeSpan.FromSeconds(1).TotalMilliseconds);
+            _timer.Elapsed += _timer_Elapsed;
+
+            OnStarTimer();
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (ActualTime.TotalSeconds > 0)
+            {
+                ActualTime = ActualTime.Subtract(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private void OnStarTimer()
+        {
+            _timer.Start();
+        }
+
+        private void OnStopTimer()
+        {
+            _timer.Stop();
+        }
+
+        private void OnResetTimer()
+        {
+            _timer.Stop();
+            ActualTime = _workDuration;
+
+            ((DelegateCommand)ResetTimerCommand).RasieCanExecuteChanged();
+        }
+
+        #region Commands
+
+        #region ResetTimerCommand
+
+        public ICommand ResetTimerCommand { get; private set; }
+
+        public bool ResetTimerCommandCanExecute()
+        {
+            return _timer.Enabled;
+        }
+
+        public void ResetTimerCommandExecuted()
+        {
+            OnResetTimer();
+        }
+
+        #endregion
+
+        #region StartPauseTimerCommand
+
+        public ICommand StartPauseTimerCommand { get; private set; }
+
+        public bool StartPauseTimerCommandCanExecute()
+        {
+            return true;
+        }
+
+        public void StartPauseTimerCommandExecuted()
+        {
+
+        }
+
+        #endregion
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}

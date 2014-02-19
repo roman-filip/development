c0fdbea1380503b9409c52fa9c19e008a08d8d2e
Roman FILIP <dicky@email.cz>
2014-02-19 21:36:22 +0100

    Added script what defines my profile

---
+++ PowerShell/Profiles/RFI_base_profile.ps1
@@ -0,0 +1,12 @@
+# Defines my basic profile
+
+# This script has to be loaded from file:
+# \\proliant23.office.hci\home\filip\My Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+# with command:
+# . 'c:\_GitHub\development\PowerShell\Profiles\RFI_base_profile.ps1'
+
+# Information about how to digitaly signed powershell script:
+# http://www.hanselman.com/blog/SigningPowerShellScripts.aspx
+
+# Create alias ig (aka install git) what adds support for git to powershell console
+New-Alias ig 'C:\Users\filip\AppData\Local\GitHub\PoshGit_8aecd991d8ccf3dc78b8cd397ee4e1595f8556d4\profile.example.ps1'

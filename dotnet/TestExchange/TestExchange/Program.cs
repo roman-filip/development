00d1da28af1de4e83a18610b8f8833cc7720127c
Roman FILIP <dicky@email.cz>
2014-08-18 21:13:23 +0200

    Added project for testing communication with Exchange

---
+++ dotnet/TestExchange/TestExchange/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Exchange.WebServices.Data;
+using TestExchange.Properties;
+
+namespace TestExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new ExchangeService(ExchangeVersion.Exchange2010);
+
+            // Setting credentials is unnecessary when you connect from a computer that is logged on to the domain.
+            service.Credentials = new WebCredentials(Settings.Default.username, Settings.Default.password, Settings.Default.domain);
+            // Or use NetworkCredential directly (WebCredentials is a wrapper around NetworkCredential).
+
+            //To set the URL manually, use the following:
+            //service.Url = new Uri("https://proliant32.hci.tetra.ppf.cz/EWS/Exchange.asmx");
+
+            //To set the URL by using Autodiscover, use the following:
+            service.AutodiscoverUrl("roman.filip@XXXXXXXXXXX.cz");
+
+            FindItemsResults<Item> findResults = service.FindItems(WellKnownFolderName.Inbox, new ItemView(10));
+            foreach (Item item in findResults.Items)
+            {
+                Console.WriteLine(item.Subject);
+            }
+        }
+    }
+}

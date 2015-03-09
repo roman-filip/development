b7067611965e67eac74f26bbde78e32463cc7e76
Roman FILIP <dicky@email.cz>
2015-03-09 10:07:27 +0100

    Added batch file for starting MakeSuperModuleMagic.ps1

---
+++ PowerShell/MakeAllSuperModuleMagic.bat
@@ -0,0 +1,3 @@
+powershell .\MakeSuperModuleMagic.ps1 -Branch cs07_dev -Release 147
+powershell .\MakeSuperModuleMagic.ps1 -Branch cs07_sp21_d4 -Release 147
+powershell .\MakeSuperModuleMagic.ps1 -Branch cs07_sp21_d5 -Release 147

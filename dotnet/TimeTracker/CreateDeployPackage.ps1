924d3bdce3df772280754fc4201136bbe037bf57
Roman FILIP <dicky@email.cz>
2015-03-14 13:53:42 +0100

    Added script for building production package

---
+++ dotnet/TimeTracker/CreateDeployPackage.ps1
@@ -0,0 +1,27 @@
+﻿function BuildApplication
+{
+    & 'C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe' .\TimeTracker.sln /t:Rebuild /p:Configuration=Release
+}
+
+function CreateZipPackage
+{
+    Add-Type -assembly "system.io.compression.filesystem"
+
+    $source = "$PSScriptRoot\bin\Release\"
+
+    $version = GetApplicationVersion
+    $destination = "$PSScriptRoot\Pichacky_$version.zip"
+    
+    [io.compression.zipfile]::CreateFromDirectory($source, $destination)
+}
+
+function GetApplicationVersion
+{
+    $assemblyInfo = Get-Content "$PSScriptRoot\TimeTracker.WindowsViews\Properties\AssemblyInfo.cs"
+    $version = ($assemblyInfo | Select-String '\d.\d.\d').Matches.Groups[0].Value
+
+    return $version
+}
+
+BuildApplication
+CreateZipPackage

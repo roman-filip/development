137ad2ef1e81269092c9440bc33db57bdc1b9f89
Roman FILIP <dicky@email.cz>
2015-03-20 09:51:51 +0100

    Change of branches for checking

--- PowerShell/GetUnmergedChanges.ps1
+++ PowerShell/GetUnmergedChanges.ps1
@@ -1,22 +1,19 @@
 ﻿$sourceSVNs = 
     @(
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs06_sp21_d1/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs06_sp21_d2/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs06_sp21_d3/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs07_sp21_d4/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs07_sp21_d5/",
-
       "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs07_sp22_d1/",
       "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs07_sp22_d2/",
       "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs07_sp22_d3/",
       "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs07_sp22_d4/",
       "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs07_sp22_d5/",
 
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs07_dev",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs08_sp23_d2/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs08_sp23_d3/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs08_sp23_d4/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs08_sp23_d5/",
 
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs06",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs07",
 
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs05")
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs06")
 
 $targetSVN = "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/trunk"
 

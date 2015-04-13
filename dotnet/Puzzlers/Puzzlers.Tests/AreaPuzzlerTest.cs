45a11b80f55a26c5d79db50fcbcd7c1d9372d610
Roman FILIP <dicky@email.cz>
2015-04-13 09:48:07 +0200

    Added project with Puzzlers where I will try to implement some algorithms

---
+++ dotnet/Puzzlers/Puzzlers.Tests/AreaPuzzlerTest.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Puzzlers.Tests
+{
+    [TestClass]
+    public class AreaPuzzlerTest
+    {
+        private AreaPuzzler _puzzler;
+
+        [TestInitialize]
+        public void InitialezeAreaPuzzler()
+        {
+            _puzzler = new AreaPuzzler();
+        }
+
+        [TestMethod]
+        public void CalculateAreaForNullBars()
+        {
+            var area = _puzzler.CalculateArea(null);
+            Assert.AreEqual(0, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForTwoBars()
+        {
+            var area = _puzzler.CalculateArea(new[] { 10, 5 });
+            Assert.AreEqual(0, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForEqualBars()
+        {
+            var area = _puzzler.CalculateArea(new[] { 5, 5, 5, 5 });
+            Assert.AreEqual(0, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForRisingBars()
+        {
+            var area = _puzzler.CalculateArea(new[] { 1, 5, 14, 20 });
+            Assert.AreEqual(0, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForFallingBars()
+        {
+            var area = _puzzler.CalculateArea(new[] { 30, 10, 8, 1 });
+            Assert.AreEqual(0, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForRisingAndFallingsBars()
+        {
+            var area = _puzzler.CalculateArea(new[] { 1, 5, 10, 8, 2 });
+            Assert.AreEqual(0, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForThreeBars3()
+        {
+            var area = _puzzler.CalculateArea(new[] { 10, 4, 4, 4 });
+            Assert.AreEqual(0, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForThreeBars()
+        {
+            var area = _puzzler.CalculateArea(new[] { 11, 5, 11 });
+            Assert.AreEqual(6, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForThreeBars2()
+        {
+            var area = _puzzler.CalculateArea(new[] { 10, 6, 8 });
+            Assert.AreEqual(2, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForFiveBars()
+        {
+            var area = _puzzler.CalculateArea(new[] { 1, 2, 10, 3, 10 });
+            Assert.AreEqual(7, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForSevenBars()
+        {
+            var area = _puzzler.CalculateArea(new[] { 1, 2, 10, 5, 7, 9, 10 });
+            Assert.AreEqual(9, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForSevenBars2()
+        {
+            var area = _puzzler.CalculateArea(new[] { 1, 2, 10, 5, 7, 7, 10 });
+            Assert.AreEqual(11, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForEightBars()
+        {
+            var area = _puzzler.CalculateArea(new[] { 1, 2, 10, 5, 7, 7, 10, 10 });
+            Assert.AreEqual(11, area);
+        }
+
+        [TestMethod]
+        public void CalculateAreaForEightBars2()
+        {
+            var area = _puzzler.CalculateArea(new[] { 1, 2, 10, 5, 7, 7, 10, 10, 10 });
+            Assert.AreEqual(11, area);
+        }
+
+        [TestMethod]
+        public void ExistCalculateAreaMethodsWithParameter2()
+        {
+            var area = _puzzler.CalculateArea(new[] { 10, 20, 80, 70, 60, 90 });
+            Assert.AreEqual(30, area);
+        }
+
+        [TestMethod]
+        public void ExistCalculateAreaMethodsWithParameter()
+        {
+            var area = _puzzler.CalculateArea(new[] { 10, 20, 80, 70, 60, 90, 40, 30, 40, 70 });
+            Assert.AreEqual(130, area);
+        }
+    }
+}
---
+++ dotnet/Puzzlers/Puzzlers/AreaPuzzler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Puzzlers
+{
+    public class AreaPuzzler
+    {
+        private int _areaStartIndex;
+
+        public int CalculateArea(int[] bars)
+        {
+            if (bars == null || bars.Length < 3)
+            {
+                return 0;
+            }
+
+            _areaStartIndex = 0;
+            var totalArea = 0;
+            while (_areaStartIndex < bars.Length)
+            {
+                totalArea += CalculateSingleArea(bars);
+            }
+
+            return totalArea;
+        }
+
+        private int CalculateSingleArea(int[] bars)
+        {
+            var area = 0;
+
+            var leftBorderIndex = GetLeftBorderIndex(bars, _areaStartIndex);
+            var leftBorderHeight = bars[leftBorderIndex];
+
+            var rightBorderIndex = GetRightBorderIndex(bars, leftBorderIndex + 1);
+            var rightBorderHeight = bars[rightBorderIndex];
+
+            var lowerBorderHeiht = Math.Min(leftBorderHeight, rightBorderHeight);
+
+            for (int i = leftBorderIndex + 1; i < rightBorderIndex; i++)
+            {
+                area += lowerBorderHeiht - bars[i];
+            }
+
+            _areaStartIndex = rightBorderIndex + 1;
+
+            return area;
+        }
+
+        private int GetLeftBorderIndex(int[] bars, int startIndex)
+        {
+            for (int i = startIndex; i < bars.Length - 1; i++)
+            {
+                if (bars[i] > bars[i + 1])
+                {
+                    return i;
+                }
+            }
+
+            return bars.Length - 1;
+        }
+
+        private int GetRightBorderIndex(int[] bars, int startIndex)
+        {
+            for (int i = startIndex; i < bars.Length - 1; i++)
+            {
+                if ((bars[i] > bars[i - 1]) && (bars[i] > bars[i + 1]))
+                {
+                    return i;
+                }
+            }
+
+            return bars[bars.Length - 1] < bars[bars.Length - 2] ? startIndex : bars.Length - 1;
+        }
+    }
+}

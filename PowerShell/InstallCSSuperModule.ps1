956ab3c8f5573cf6cd1c981d5f73f310b176c520
Roman FILIP <dicky@email.cz>
2015-05-14 13:55:07 +0200

    Added script that installs suitable supermodule for CS .NET branch

---
+++ PowerShell/InstallCSSuperModule.ps1
@@ -0,0 +1,41 @@
+﻿$supermodulesDir = "\\proliant03dev\HomeR_Deploy\CS\Supermoduls\"
+
+function GetBranchName
+{
+    $url = $svnInfo.info.entry.url
+    $urlElements = $url.Split("/")
+    $branch = $urlElements[$urlElements.Length - 2]
+
+    return $branch
+}
+
+function UnzipSuperModule
+{
+    $searchFilter = "SuperModul_" + $branchName + "_*.zip"
+
+    $zippedSupermodule = Get-ChildItem $supermodulesDir -Filter $searchFilter | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1
+    if ($zippedSupermodule -eq $null)
+    {
+        return "ERROR: Supermodule with name " + $searchFilter + " doesn't found"
+    }
+
+    $fileToUnzip = $supermodulesDir + $zippedSupermodule
+
+    unzip -o $fileToUnzip -d $localSuperModuleDir
+}
+
+function InstallSuperModule
+{
+    $superModule = Get-ChildItem $localSuperModuleDir -Filter "HCF_SuperModul_*.dll" | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1
+
+    regsvr32 /s $superModule.FullName
+}
+
+
+$svnInfo = [xml](svn info --xml)
+$dotNetDir = $svnInfo.info.entry.'wc-info'.'wcroot-abspath'
+$localSuperModuleDir = $dotNetDir + "\Lib\SuperModul"
+
+$branchName = GetBranchName
+UnzipSuperModule
+InstallSuperModule

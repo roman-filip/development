6d034ec094287639ea5e69c8ad2111422ad89cb4
Roman FILIP <dicky@email.cz>
2015-09-02 15:08:20 +0200

    Adjusted for new location of SuperModule

--- PowerShell/InstallCSSuperModule.ps1
+++ PowerShell/InstallCSSuperModule.ps1
@@ -1,4 +1,5 @@
 ﻿$supermodulesDir = "\\proliant03dev\HomeR_Deploy\CS\Supermoduls\"
+$vb6ModulesDir = "v:\_DEVELOPMENT\MODULY\CS\200_PRERELEASE_TEST\"
 
 function GetBranchName
 {
@@ -9,6 +10,8 @@ function GetBranchName
     return $branch
 }
 
+# This method is not anymore used
+# It was used for copying of files from build server
 function UnzipSuperModule
 {
     $searchFilter = "SuperModul_" + $branchName + "_*.zip"
@@ -24,6 +27,12 @@ function UnzipSuperModule
     unzip -o $fileToUnzip -d $localSuperModuleDir
 }
 
+function CopySuperModule
+{
+    $superModulePath = $vb6ModulesDir + "CS$releaseNumber\$branchName\HOMER.VB6\HCF_SuperModul_*.dll"
+    Copy-Item -Path $superModulePath -Destination $localSuperModuleDir
+}
+
 function InstallSuperModule
 {
     $superModule = Get-ChildItem $localSuperModuleDir -Filter "HCF_SuperModul_*.dll" | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1
@@ -31,11 +40,33 @@ function InstallSuperModule
     regsvr32 /s $superModule.FullName
 }
 
+function GetReleseNumber
+{
+    $assemblyInfoFile = $dotNetDir + "\DafneShell\Properties\AssemblyInfo.cs"
+    $assemblyInfo = Get-Content -Path $assemblyInfoFile
+    
+    foreach($line in $assemblyInfo)
+    {
+        if ($line -match 'AssemblyVersion\("\w*')
+        {   # if file contains $AssemblyVersion then return its number
+            if ($Matches[0] -match '\d+')
+            {
+                return $Matches[0]
+            }
+        }
+    }
+
+    throw "Cannot get release number from the \DafneShell\Properties\AssemblyInfo.cs file"
+}
+
 
 $svnInfo = [xml](svn info --xml)
 $dotNetDir = $svnInfo.info.entry.'wc-info'.'wcroot-abspath'
 $localSuperModuleDir = $dotNetDir + "\Lib\SuperModul"
 
+$releaseNumber = GetReleseNumber
 $branchName = GetBranchName
-UnzipSuperModule
+
+#UnzipSuperModule
+CopySuperModule
 InstallSuperModule

85640cbb24a13709188b692d2e50f511d3b13e0d
Roman FILIP <dicky@email.cz>
2015-09-03 16:06:38 +0200

    Added template for Visual Studio what generates basic structure of MVVM implementation used in HCI

---
+++ dotnet/VSTemplates/VSTemplates/TemplateContent/View.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Practices.ObjectBuilder;
+
+namespace $rootnamespace$.$fileinputname$
+{
+    /// <summary>
+    /// Interaction logic for $fileinputname$View.xaml
+    /// </summary>
+    public partial class $fileinputname$View
+    {
+        [InjectionConstructor]
+        public $fileinputname$View([CreateNew]$fileinputname$ViewModel viewModel)
+        {
+            InitializeComponent();
+
+            SetViewModel(viewModel);
+        }
+    }
+}
---
+++ dotnet/VSTemplates/VSTemplates/TemplateContent/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Net.Homecredit.Infrastructure.Module.WPF.MVVM;
+using Net.Homecredit.Infrastructure.Module.WPF.Validations;
+
+namespace $rootnamespace$.$fileinputname$
+{
+    public class $fileinputname$ViewModel : BaseViewModel
+    {
+        #region Overrides of BaseViewModel
+
+        public override void BindData(params object[] bindViewParameters)
+        {
+            base.BindData(bindViewParameters);
+        }
+
+        public override void OnLoad()
+        {
+            base.OnLoad();
+        }
+
+        protected override void ValidateCustom(ValidationResultCollection validationResults)
+        {
+            base.ValidateCustom(validationResults);
+        }
+
+        #endregion
+    }
+}

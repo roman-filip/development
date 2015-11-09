7d610f3e5770660b483a9e3ea90d1eeab4870c5d
Roman FILIP <dicky@email.cz>
2015-11-09 22:18:28 +0100

    Added new gadget application for displaying of images from specified directory

---
+++ dotnet/PhotoGadget/PhotoGadget/Views/PhotoGadgetView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace PhotoGadget.Views
+{
+    /// <summary>
+    /// Interaction logic for PhotoGadgetView.xaml
+    /// </summary>
+    public partial class PhotoGadgetView : Window
+    {
+        public PhotoGadgetView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+    }
+}
---
+++ dotnet/PhotoGadget/PhotoGadget/Views/PhotoGadgetViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Threading;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using PhotoGadget.Properties;
+
+namespace PhotoGadget.Views
+{
+    public class PhotoGadgetViewModel : ViewModelBase
+    {
+        private Random _random = new Random();
+
+        private DispatcherTimer _timer;
+
+        private List<string> _images;
+
+        private Stack<int> _displayedIndexes = new Stack<int>();
+
+        private string _imagePath;
+
+        public string ImagePath
+        {
+            get { return _imagePath; }
+            set { Set(ref _imagePath, value); }
+        }
+
+        public RelayCommand ShowNextImageCommand { get; private set; }
+
+        public RelayCommand ShowPreviousImageCommand { get; private set; }
+
+        public PhotoGadgetViewModel()
+        {
+            ShowNextImageCommand = new RelayCommand(OnShowNextImage);
+            ShowPreviousImageCommand = new RelayCommand(OnShowPreviousImage, () => _displayedIndexes.Count > 0);
+
+            LoadImages();
+            OnShowNextImage();
+            InitTimer();
+        }
+
+        private void LoadImages()
+        {
+            var imagesPath = Settings.Default.ImagesPath;
+            _images =
+                Directory.EnumerateFiles(imagesPath, "*.*", SearchOption.AllDirectories)
+                .Where(s => s.EndsWith(".jpg") || s.EndsWith(".jpeg") || s.EndsWith(".gif") || s.EndsWith(".bmp") || s.EndsWith(".png"))
+                .ToList();
+        }
+
+        private void InitTimer()
+        {
+            _timer = new DispatcherTimer { Interval = Settings.Default.RefreshInterval };
+            _timer.Tick += (sender, e) => OnShowNextImage();
+
+            _timer.Start();
+        }
+
+        private void OnShowNextImage()
+        {
+            var imageIndex = _random.Next(0, _images.Count);
+
+            _displayedIndexes.Push(imageIndex);
+            ImagePath = _images[imageIndex];
+
+            ShowPreviousImageCommand.RaiseCanExecuteChanged();
+        }
+
+        private void OnShowPreviousImage()
+        {
+            var imageIndex = _displayedIndexes.Pop();
+            ImagePath = _images[imageIndex];
+
+            ShowPreviousImageCommand.RaiseCanExecuteChanged();
+        }
+    }
+}

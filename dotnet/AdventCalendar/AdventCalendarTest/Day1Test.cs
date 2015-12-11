5b6e5b3595e6ed7404328fcaeb24bf66f8b5e488
Roman FILIP <dicky@email.cz>
2015-12-11 21:11:54 +0100

    Added project for solution of Advent of code calendar

---
+++ dotnet/AdventCalendar/AdventCalendarTest/Day1Test.cs
@@ -0,0 +1,25 @@
+﻿using AdventCalendar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventCalendarTest
+{
+    [TestClass]
+    public class Day1Test
+    {
+        [TestMethod]
+        public void Part1Test()
+        {
+            var day1 = new Day1();
+            var answer = day1.Part1();
+            Assert.AreEqual(232, answer);
+        }
+
+        [TestMethod]
+        public void Part2Test()
+        {
+            var day1 = new Day1();
+            var answer = day1.Part2();
+            Assert.AreEqual(1783, answer);
+        }
+    }
+}

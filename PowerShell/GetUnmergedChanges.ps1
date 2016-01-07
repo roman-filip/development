921ea6eff68f5e9b1508d25fb1c368e14e6938b6
Roman FILIP <dicky@email.cz>
2016-01-07 14:51:41 +0100

    Change of branches for checking

--- PowerShell/GetUnmergedChanges.ps1
+++ PowerShell/GetUnmergedChanges.ps1
@@ -1,19 +1,19 @@
 ﻿$sourceSVNs = 
     @(
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs15_sp41_d1/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs15_sp41_d2/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs15_sp41_d3/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs15_sp41_d4/",
-
       "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs15_sp42_d1/",
       "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs15_sp42_d2/",
       "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs15_sp42_d3/",
       "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs15_sp42_d4/",
 
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs14",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs16_sp43_d1/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs16_sp43_d2/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs16_sp43_d3/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs16_sp43_d4/",
 
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs13",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs13_fix")
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs14",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs14_fix",
+
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs15")
 
 $targetSVN = "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/trunk"
 

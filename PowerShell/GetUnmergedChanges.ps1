d7e0be7cf3b741ae7acabbc9e401962ebc62b3eb
Roman FILIP <dicky@email.cz>
2016-03-03 13:44:33 +0100

    Change of branches for checking

--- PowerShell/GetUnmergedChanges.ps1
+++ PowerShell/GetUnmergedChanges.ps1
@@ -1,19 +1,20 @@
 $sourceSVNs = 
     @(
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs16_sp44_d1/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs16_sp44_d2/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs16_sp44_d3/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs16_sp44_d4/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs17_sp46_d1/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs17_sp46_d2/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs17_sp46_d3/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs17_sp46_d4/",
 
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs17_sp45_d1/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs17_sp45_d2/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs17_sp45_d3/",
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs17_sp45_d4/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs18_sp47_d1/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs18_sp47_d2/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs18_sp47_d3/",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs18_sp47_d4/",
 
       "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs15",
       "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs15_fix",
       
-      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs16")
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs16",
+      "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/branches/cs16_fix")
 
 $targetSVN = "https://subversion.homecredit.net/repos/dotnet/dotnetclient/cs/trunk"
 
